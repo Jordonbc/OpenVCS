3c8e212fd667045277cef983ea8e88effeea1785
Jordon <jordon@bbgames.dev>
2026-05-08 19:39:06 +0100

    Update workarounds.rs

--- Backend/src/workarounds.rs
+++ Backend/src/workarounds.rs
@@ -109,7 +109,7 @@ pub fn main_window_browser_args(performance: &Performance) -> Option<String> {
     }
 
     Some(
-        "--disable-gpu --disable-gpu-compositing --disable-features=msWebOOUI,msPdfOOUI,msSmartScreenProtection"
+        "--disable-gpu --disable-gpu-compositing --disable-features=msWebOOUI,msPdfOOUI"
             .to_string(),
     )
 }
